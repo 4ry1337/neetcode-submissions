3091844e51b3636aff3d578f9c273ce88eebe99f
Rakhat Yskak <yskak.rakhat@gmail.com>
2026-07-06 17:36:55 +0500

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+  string longestCommonPrefix(vector<string>& strs) {
+    string res = "";
+    for (int i{}; i < strs[0].size(); ++i) {
+      char& c = strs[0][i];
+      for (auto& str : strs) {
+        if (i >= str.size() || str[i] != c) {
+          return res;
+        }
+      }
+      res += c;
+    }
+    return res;
+  }
+};
