eeff867614b6423895d47626cde9c870d2100757
Rakhat Yskak <yskak.rakhat@gmail.com>
2026-07-22 01:31:29 +0500

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,16 @@
+class KthLargest {
+ private:
+  priority_queue<int, vector<int>, greater<int>> pq;
+  int k;
+
+ public:
+  KthLargest(int k, vector<int>& nums) : pq(nums.begin(), nums.end()), k(k) {
+    while (pq.size() > k) pq.pop();
+  }
+
+  int add(int val) {
+    pq.push(val);
+    if (pq.size() > k) pq.pop();
+    return pq.top();
+  }
+};
