9d900e4dddc5d6933e2abb9b561d5631cd44a689
Rakhat Yskak <yskak.rakhat@gmail.com>
2026-07-31 02:08:45 +0500

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.cpp
@@ -0,0 +1,45 @@
+class WordDictionary {
+ private:
+  struct Node {
+    unordered_map<char, Node*> next;
+    bool is_end = false;
+  };
+  Node* root;
+
+ public:
+  WordDictionary() { root = new Node(); }
+
+  void addWord(string word) {
+    Node* curr = root;
+    for (auto& c : word) {
+      if (!curr->next.contains(c)) {
+        curr->next[c] = new Node();
+      }
+      curr = curr->next[c];
+    }
+    curr->is_end = true;
+  }
+
+  bool search_mp(Node* node, string& word, size_t idx) {
+    for (size_t i{idx}; i < word.size(); ++i) {
+      auto& c = word[i];
+      if (c == '.') {
+        for (auto& [ch, mp] : node->next) {
+          if (search_mp(mp, word, i + 1)) return true;
+        }
+      }
+      if (!node->next.contains(c)) return false;
+      node = node->next[c];
+    }
+    return node->is_end;
+  }
+
+  bool search(string word) { return search_mp(root, word, 0); }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
